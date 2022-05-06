acaa17a2bf3e6ad27874ece5a397e5b4f754b3dd
IlyaIvavov <85294343+IlyaIvavov@users.noreply.github.com>
2022-05-06 13:56:09 +0300

    2

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,10 @@
-Selection
+var swiper = new Swiper('.swiper', {
+	direction: 'vertical',
+	slidesPerView: 1,
+	spaceBetween: 30,
+	mousewheel: true,
+	pagination: {
+	  el: '.swiper-pagination',
+	  clickable: true,
+	},
+  });
