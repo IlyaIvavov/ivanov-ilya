45aa7dca1c46d806a16535036ae0d8bb3e68ce0c
IlyaIvavov <85294343+IlyaIvavov@users.noreply.github.com>
2022-05-13 14:40:50 +0300

    Правки в swipper
    Почти все переделал

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,8 @@
-const pageSlider = new Swiper('.page', {
-	wrapperClass: '.page__wrapper',
-	sliderClass: '.page__screen',
+// Swiper
+
+let pageSlider = new Swiper('.page', {
+	wrapperClass: "page__wrapper",
+	slideClass: "page__screen",
 	direction: 'vertical',
 	slidesPerView: 'auto',
 	parallax: true,
@@ -16,7 +18,7 @@ const pageSlider = new Swiper('.page', {
 	},
 
 	watchOverflow: true,
-	speed: 400,
+	speed: 800,
 	observer: true,
 	observeParents: true,
 	observeSlideChildren: true,
@@ -25,13 +27,55 @@ const pageSlider = new Swiper('.page', {
 		el: '.page__pagination',
 		type: 'bullets',
 		clickable: true,
-		bulletClass: '.page__bullet',
-		bulletActiveClass: '.page__bullet_active',
+		bulletClass: "page__bullet",
+		bulletActiveClass: "page__bullet_active",
 	},
 
 	scrollbar: {
 		el: '.page__scroll',
-		dragClass: '.page_drag-scroll',
+		dragClass: "page__drag-scroll",
 		draggable: true,
 	},
+
+	init: false,
+
+	on: {
+		init: function () {
+			menuSlider();
+		},
+		slideChange: function () {
+			menuSliderRemove();
+			menuLinks[pageSlider.realIndex].classList.add('_active');
+		},
+	},
 });
+
+// Nav Menu
+
+let menuLinks = document.querySelectorAll('.menu__link');
+
+function menuSlider() {
+	if (menuLinks.length > 0) {
+		menuLinks[pageSlider.realIndex].classList.add('_active');
+		for (let index = 0; index < menuLinks.length; index++) {
+			const menuLink = menuLinks[index];
+			menuLink.addEventListener("click", function (e) {
+				menuSliderRemove();
+				pageSlider.slideTo(index, 800);
+				menuLink.classList.add('_active');
+				e.preventDefault();
+			});
+		}
+	}
+}
+ 
+function menuSliderRemove() {
+	let menuLinkActive = document.querySelector('.menu__link._active');
+	if (menuLinkActive) {
+		menuLinkActive.classList.remove('_active');
+	}
+}
+
+pageSlider.init();
+
+// Animate
